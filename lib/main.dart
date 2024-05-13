62ecf175f95b0b4770c10ed8b8db536015edcb04
kakovets <victorentino@gmail.com>
2024-05-13 23:56:07 +0300

    it is what it is

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'package:dream_craft/providers/auth_provider.dart';
+import 'package:dream_craft/providers/category_provider.dart';
+import 'package:dream_craft/screens/wrapper.dart';
+import 'package:dream_craft/screens/auth/login_screen.dart';
+import 'package:dream_craft/theme/theme_provider.dart';
+import 'package:dream_craft/utils/utils.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'generated/codegen_loader.g.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('uk')],
+      path: 'assets/translations',
+
+      fallbackLocale: const Locale('en'),
+      assetLoader: const CodegenLoader(),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: ((context) => Auth()),
+          ),
+          ChangeNotifierProvider(
+            create: ((context) => CategoryProvider()),
+          ),
+          ChangeNotifierProvider(
+            create: ((context) => ThemeProvider()..getSavedTheme()),
+          ),
+        ],
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,12 +44,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('DreamCraft'),
-        ),
-      ),
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Future<void> _attemptAuthentication() async {
+    String? key = await Utils.getToken();
+
+    Provider.of<Auth>(context, listen: false).getUser(key);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _attemptAuthentication();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Auth>(
+      builder: (context, authProvider, child) {
+        if (authProvider.authenticated) {
+          return const Wrapper();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
